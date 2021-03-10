108f97843b3da27fdf37fa2927a595f5123ed663
Philippe Lavigueur <plavigueur@coveo.com>
2021-03-10 15:15:20 -0500

    Fix Indentation + Using config file

--- src/webparts/coveoIntegration/SearchInterfaceTemplate.ts
+++ src/webparts/coveoIntegration/SearchInterfaceTemplate.ts
@@ -1,7 +1,7 @@
 export default class SearchInterfaceTemplate {
-    public static templateHtml: string = `
+  public static templateHtml: string = `
     <div id="custom-coveo-main-search-interface">
-        <hosted-search-page id="hsp"></hosted-search-page>
+      <hosted-search-page id="hsp"></hosted-search-page>
     </div>
-    `;
+  `;
 }
