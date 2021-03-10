0a0ac344bdcd82aa8965f1c93e04c7eef9479922
Philippe Lavigueur <plavigueur@coveo.com>
2021-03-10 15:48:46 -0500

    Removed config file

--- src/webparts/coveoIntegration/CoveoIntegrationWebPart.ts
+++ src/webparts/coveoIntegration/CoveoIntegrationWebPart.ts
@@ -5,7 +5,6 @@ import { sp } from '@pnp/sp/presets/all';
 import { HostedSearchPage } from '@coveops/hosted-search-page';
 import * as Coveo from 'coveo-search-ui';
 
-import * as coveoConfig from './../../../config/coveo-config.json';
 import SearchInterfaceTemplate from './SearchInterfaceTemplate';
 
 export interface ICoveoIntegrationWebPartProps {
@@ -61,6 +60,12 @@ export default class CoveoIntegrationWebPart extends BaseClientSideWebPart<ICove
   }
   
   private defineCoveoCustomParameters() {
-    Coveo['customParameters'] = Coveo['customParameters'] || coveoConfig;
+    Coveo['customParameters'] = Coveo['customParameters'] || {
+      coveoRestURI: 'https://platform.cloud.coveo.com/rest/search', // The URI of the Coveo service
+      coveoOrgId: 'myorgid', // The id of the Coveo Cloud organization
+      coveoSearchAPIKey: 'xxxxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxxx', // The API key used for the search request (search and usage analytics)
+      coveoPageAPIKey: 'xxxxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxxx', // The API key used to get the page content (search page API)
+      coveoPageId: 'xxxxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxxx', // The id of the search page,
+    };
   }
 }
